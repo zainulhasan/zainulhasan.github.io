0a22be1fc48d48abb3f97802c0824ef94fcbe3e1
Syed Zain Ul Hasan <hassan9224@gmail.com>
2021-04-27 23:06:28 +0500

    Updates

---
+++ precache-manifest.d2f0b7b8dc2a2f8096287395ec232d55.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8038594e7141364fcbdc3e635ba0ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c24e31d5f029b046730",
+    "url": "/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "4ec34c5f500b3287ac3f",
+    "url": "/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "cf9597aaf150e51e6431",
+    "url": "/static/css/main.2b62e6a7.chunk.css"
+  },
+  {
+    "revision": "4c65a6e428144ac095da",
+    "url": "/static/js/2.763c2b7e.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/static/js/2.763c2b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c24e31d5f029b046730",
+    "url": "/static/js/3.9eddd5f5.chunk.js"
+  },
+  {
+    "revision": "4ec34c5f500b3287ac3f",
+    "url": "/static/js/4.0300fac5.chunk.js"
+  },
+  {
+    "revision": "cf9597aaf150e51e6431",
+    "url": "/static/js/main.86928888.chunk.js"
+  },
+  {
+    "revision": "64b1d03718ba01145f46",
+    "url": "/static/js/runtime-main.1b839141.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "229bf344aecafde616842f54326e3ecb",
+    "url": "/static/media/advisorycloud.229bf344.png"
+  },
+  {
+    "revision": "254208c01172326aff312fd3b1278828",
+    "url": "/static/media/bahria_logo.254208c0.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "8ac1627dbc1f19a729fbb3c359b9f0e6",
+    "url": "/static/media/theskillsat.8ac1627d.webp"
+  },
+  {
+    "revision": "dcdd3df00e38b8c2079e643c26563aa9",
+    "url": "/static/media/wonderdads.dcdd3df0.jpg"
+  }
+]);
